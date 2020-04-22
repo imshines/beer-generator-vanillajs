41e17fc8d06fd0f83b1c891c65189471ef2fa10e
Mohan Kumar <46491614+imshines@users.noreply.github.com>
2020-04-22 12:51:14 +0530

    complete :)

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const beerImage = document.querySelector('.beer-image')
+    const beerTitle = document.querySelector('.beer-title');
+    const beerTagline = document.querySelector('.beer-tagline');
+    const beerDescription = document.querySelector('.beer-description');
+    const beerDate = document.querySelector('.beer-date');
+
+    let url = "https://api.punkapi.com/v2/beers/random";
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        const name = data[0].name;
+        const tagline = data[0].tagline;
+        const first_brewed = data[0].first_brewed;
+        const description = data[0].description;
+        const image = data[0].image_url;
+
+        beerTitle.innerHTML = name;
+        beerTagline.innerHTML = tagline;
+        beerDescription.innerHTML = description;
+        beerDate.innerHTML = first_brewed;
+        beerImage.src = image;
+    })
+
+})
